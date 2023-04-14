a3d4029be66f2dcae6294cc2dcf788a3b8353bae
vsPEach <pers1kwtf@gmail.com>
2023-04-14 20:54:13 +0300

    add flag parse

--- GatewayService/cmd/main.go
+++ GatewayService/cmd/main.go
@@ -1,10 +1,19 @@
 package main
 
 import (
+	"flag"
 	"github.com/vsPEach/LMS_subsystem/DistributorService/config"
 	"github.com/vsPEach/LMS_subsystem/DistributorService/internal/app "
 	"log"
 )
+
+var (
+	path string
+)
+
+func init() {
+	flag.StringVar(&path, "path", "./config/config.yml", "")
+}
 
 func main() {
 	cfg, err := config.NewConfig("")
