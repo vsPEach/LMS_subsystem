504218bc1b28f5dc4df0905a1e1b5bc029ce26b1
vsPEach <pers1kwtf@gmail.com>
2023-04-14 20:38:58 +0300

    update

--- GatewayService/internal/app /app.go
+++ GatewayService/internal/app /app.go
@@ -1,1 +1,12 @@
-package app_
+package app
+
+import (
+	"github.com/vsPEach/LMS_subsystem/DistributorService/config"
+	"github.com/vsPEach/LMS_subsystem/DistributorService/internal/server"
+)
+
+func Run(conf config.Config) {
+	handler := new(server.Handler)
+	serv := server.NewServer(conf.Server, handler)
+	serv.Start()
+}
